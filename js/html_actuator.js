09dd327f524d747fdd7acc630689bbab73f6a824
Fluff 5 <76451731+Fluff5@users.noreply.github.com>
2025-06-15 21:59:17 +0100

    Update html_actuator.js

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -81,6 +81,8 @@ HTMLActuator.prototype.addTile = function (tile) {
   if (tile.value === -20) inner.textContent = 20;
   if (tile.value === -21) inner.textContent = 21;
   if (tile.value === -24) inner.textContent = 24;
+  if (tile.value === -28) inner.textContent = 28;
+  if (tile.value === -50) inner.textContent = 50;
   if (tile.value === -66) inner.textContent = 66;
 
 
