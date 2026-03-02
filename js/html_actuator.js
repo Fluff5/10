b1bdf8fefc51d86511db625cf588816c7b65aaa8
Fluff 5 <76451731+Fluff5@users.noreply.github.com>
2026-03-02 17:35:14 +0000

    64 tiles

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -68,14 +68,7 @@ HTMLActuator.prototype.addTile = function (tile) {
 
   inner.classList.add("tile-inner");
   inner.textContent = tile.value;
-  if (tile.value === -17) inner.textContent = 17;
-  if (tile.value === -21) inner.textContent = 21;
-  if (tile.value === -23) inner.textContent = 23;
-  if (tile.value === -24) inner.textContent = 24;
-  if (tile.value === -33) inner.textContent = "3 3";
-  if (tile.value === -63) inner.textContent = 63;
-  if (tile.value === -70) inner.textContent = 70;
-
+  
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
     window.requestAnimationFrame(function () {
