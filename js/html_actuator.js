6c29a33b95fe22e7430f5cf52cc92e2398225d3f
Fluff 5 <76451731+Fluff5@users.noreply.github.com>
2026-04-06 10:50:30 +0100

    new tiles

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -71,6 +71,7 @@ HTMLActuator.prototype.addTile = function (tile) {
   if (tile.value === -21) inner.textContent === 21;
   if (tile.value === -23) inner.textContent === 23;
   if (tile.value === -33) inner.textContent === "3 3";
+  if (tile.value === -55) inner.textContent === 55;
   if (tile.value === -63) inner.textContent === 63;
   if (tile.value === -70) inner.textContent === 70;
   if (tile.value === -75) inner.textContent === 75;
